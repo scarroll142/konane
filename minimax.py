d3567de877d4a188d6f98714d9d1a842e6a95296
Tommy Russoniello <tommy.russoniello@gmail.com>
2018-04-15 00:45:10 -0400

    Added alpha-beta pruning to minimax algorithm

--- minimax.py
+++ minimax.py
@@ -1,3 +1,11 @@
+### Authors:     Sam Carroll and Tommy Russoniello
+### Professor:   Dr. Schwartz
+### Class:       CSCI 450 Spring 2018
+### Assignment:  Minimax Konane
+### Date:        April 17, 2018
+
+from konane import *
+
 class MinimaxPlayer(Konane, Player):
     """
     Plays best move determined by minimax algorithm.
@@ -14,28 +22,40 @@ class MinimaxPlayer(Konane, Player):
         self.name = "Beep Boop Bop"
 
     def getMove(self, board):
+        """
+        Given the current board, should return a valid move.
+        """
         moves = self.generateMoves(board, self.side)
 
         if not moves:
             return []
 
         values = []
+        alpha = -float("inf")
         for move in moves:
-            values.append(self.minimax(self.nextBoard(board, self.side, move), 1))
+            values.append(self.minimax(self.nextBoard(board, self.side, move), 1, alpha, float("inf")))
+
+            if max(values) > alpha:
+                alpha = max(values)
 
         return moves[values.index(max(values))]
 
     def eval(self, board):
+        """
+        Determines heuristic value of given board.
+        """
         return self.movesCount(board, self.opponent(self.side)) * -1
-        #complete this – this will be your evaluation function. High values should be good for max.
 
     def movesCount(self, board, player):
         """
-        Determines heuristic value of the number of possible moves for given player for current board
+        Determines heuristic value of the number of possible moves for given player for current board.
         """
         return len(self.generateMoves(board, player))
 
     def extendPath(self, board, side):
+        """
+        Returns resulting boards for all possible legal moves from given board for given player.
+        """
         moves = self.generateMoves(board, side)
         boards = []
         for move in moves:
@@ -43,7 +63,10 @@ class MinimaxPlayer(Konane, Player):
 
         return boards
 
-    def minimax(self, board, depth):
+    def minimax(self, board, depth, alpha, beta):
+        """
+        Uses Minimax algorithm to give best possible board heuristic value up to end of game/given search depth assuming optimal opponent.
+        """
         if depth >= self.limit:
             return self.eval(board)
 
@@ -61,8 +84,22 @@ class MinimaxPlayer(Konane, Player):
                 return float("inf")
 
         values = []
+        newAlpha = alpha
+        newBeta = beta
         for nextBoard in nextBoards:
-            values.append(self.minimax(nextBoard, depth + 1))
+            if values:
+                if isMax:
+                    if max(values) >= beta:
+                        break
+                    if max(values) > alpha:
+                        newAlpha = max(values)
+                else:
+                    if min(values) <= alpha:
+                        break
+                    if min(values) < beta:
+                        newBeta = min(values)
+
+            values.append(self.minimax(nextBoard, depth + 1, newAlpha, newBeta))
 
         if isMax:
             return max(values)
